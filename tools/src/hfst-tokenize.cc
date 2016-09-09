5a33841f5e78fdbadea8ce6284e8b189256c69a3
Sam Hardwick <sam.hardwick@iki.fi>
2016-09-09 08:53:57 +0300

    Don't separate tokens with empty lines in plain tokenization mode

--- tools/src/hfst-tokenize.cc
+++ tools/src/hfst-tokenize.cc
@@ -488,7 +488,7 @@ void print_location_vector(LocationVector const & locations, std::ostream & outs
         if (print_weights) {
             outstream << "\t" << locations.at(0).weight;
         }
-        outstream << std::endl << std::endl;
+        outstream << std::endl;
     } else if (output_format == cg && locations.size() != 0) {
         // Print the cg cohort header
         outstream << "\"<" << locations.at(0).input << ">\"" << std::endl;
