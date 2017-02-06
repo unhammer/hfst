1cd8cb85b86eae0e9d9e0e4c26617e1e83769eae
eaxelson <eaxelson@users.noreply.github.com>
2017-02-06 10:54:12 +0200

    Add help message.

--- scripts/make-libhfst-dist.sh
+++ scripts/make-libhfst-dist.sh
@@ -6,6 +6,19 @@
 # distribution tarball and second one the directory where alternative files
 # for LGPL distribution can be found (folder lgpl-release in hfst repository).
 
+if [ "$1" = "--help" -o "$1" = "-h" ]; then
+    echo ""
+    echo "A script for creating a minimal LGPL package for HFST which contains"
+    echo "only openfst back-ends, HFSTlibrary and python bindings."
+    echo ""
+    echo "Usage: make-libhfst-dist.sh DIST LGPL_DIR"
+    echo ""
+    echo "DIST:     the normal HFST distribution tarball"
+    echo "LGPL_DIR: the folder where alternative files for LGPL distribution can be found"
+    echo ""
+    exit 0
+fi
+
 if [ ! -e "$1" ]; then
     echo "File "$1" does not exist"
     exit 1
