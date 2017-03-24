d8db5525f8c65abb1502ea2bcc49d4db4465cdd2
eaxelson <erik.axelson@helsinki.fi>
2017-03-24 14:19:09 +0200

    Update man pages. Remove man pages of tools that are no longer supported. Update copyright year.

--- tools/src/hfst-commandline.cc
+++ tools/src/hfst-commandline.cc
@@ -867,7 +867,7 @@ print_version()
 {
   // c.f. http://www.gnu.org/prep/standards/standards.html#g_t_002d_002dversion
     fprintf(message_out, "%s %s (" PACKAGE_STRING ")\n"
-             "Copyright (C) 2016 University of Helsinki,\n"
+             "Copyright (C) 2017 University of Helsinki,\n"
              "License GPLv3: GNU GPL version 3 "
              "<http://gnu.org/licenses/gpl.html>\n"
              "This is free software: you are free to change and "
